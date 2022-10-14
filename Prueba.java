5adc859aab6335ad660a0065024f58c93e387f4a
JesusRiestra <jriestraricote@gmail.com>
2022-10-14 12:14:47 +0200

    Ultimo commit

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Riestra Ricote");
 		System.out.println("18 a\u00f1os - 23/02/2004");
 		System.out.println("Mi comida favorita son los espaguetis");
+		System.out.println("Mi grupo de m\u00fasica favorito es Imagine Dragons");
 
 	}
 
